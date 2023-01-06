6363cdfe46e64c4aa3c50339f942adb854acc6bd
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-06 21:29:56 +0000

    Create a function for unpacking compressed file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use flate2::read::GzDecoder;
 use std::fs::File;
+use tar::Archive;
 
-fn main() -> Result<(), std::io::Error> {
-    // open file
-    File::open("archive.tar.gz").unwrap();
-    // create
+fn main() {
+    unpack_archive("./archive.tar.gz", "./midjourney").unwrap();
+}
+
+fn unpack_archive(archive_filepath: &str, outpath: &str) -> Result<(), std::io::Error> {
+    let f = File::open(archive_filepath)?;
+    let gz = GzDecoder::new(f);
+    let mut archive = Archive::new(gz);
+
+    archive.unpack(outpath)?;
     Ok(())
 }
